68bea95c4940ac7f34552d5e2b2972ce44fdd393
Lucas Quintana <lucasgabrielquintana@gmail.com>
2022-11-05 04:50:34 -0300

    Correcciones con test

--- snake/src/actions/actions.rb
+++ snake/src/actions/actions.rb
@@ -14,28 +14,29 @@ module Actions
 
     private
 
-    def calc_next_position(state)
-        new_positions = state.snake.positions.first
+    def self.calc_next_position(state)
+        curr_position = state.snake.positions.first
+        
         case state.next_direction
-            when UP 
+            when Model::Direction::UP 
                 # decrementar fila
                 
 
                 new_position = Model::Coord.new(curr_position.row - 1, curr_position.col)
-            when RIGHT
+            when Model::Direction::RIGHT
                 # incrementar columna
                 new_position = Model::Coord.new(curr_position.row, curr_position.col + 1)
-            when DOWN
+            when Model::Direction::DOWN
                 # incrementar fila
                 new_position = Model::Coord.new(curr_position.row + 1, curr_position.col)
-            when LEFT
+            when Model::Direction::LEFT
                 # decrementar column
                 new_position = Model::Coord.new(curr_position.row, curr_position.col - 1)
         end
     end
-    def position_is_valid?(state, position)
+    def self.position_is_valid?(state, position)
         #verificar que este en la grilla
-        is_invalid = ((position.row >= state.grid.row || position.row < 0) || (position.col >= state.grid.cols || positiion.col < 0))
+        is_invalid = ((position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.cols || position.col < 0))
 
         return false if is_invalid
         #verificar que no este superponiendo a la serpiente
@@ -43,14 +44,15 @@ module Actions
         return !(state.snake.positions.include? position)
     end
 
-    def move_snake_to(state, next_position)
+    def self.move_snake_to(state, next_position)
         #[]
         #[(1,1)]
-        new_position = [next_position] + state.snake.positiions[0...-1]
-        state.snake.positiion = new_positions
+        new_position = [next_position] + state.snake.positions[0...-1]
+        state.snake.positions = new_position
+        state
     end
 
-    def end_game(game)
+    def self.end_game(game)
         state.game_finished = true
         state
     end
